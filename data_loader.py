f49f11ea02c22276b7e39cc16cdb5c491a01b1d8
Nguyen Khanh <khanh1144kb@gmail.com>
2022-11-15 08:03:08 +0700

    1st

---
+++ data_loader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+# Make display smaller
+def data_loader():
+    pd.options.display.max_rows = 10
+    unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+    users = pd.read_csv('ml-1m/users.dat', sep='::',
+    header=None, names=unames, engine='python',encoding='ISO-8859-1')
+    rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
+    ratings = pd.read_csv('ml-1m/ratings.dat', sep='::',
+    header=None, names=rnames, engine='python',encoding='ISO-8859-1')
+    mnames = ['movie_id', 'title', 'genres']
+    movies = pd.read_csv('ml-1m/movies.dat', sep='::',
+    header=None, names=mnames, engine='python',encoding='ISO-8859-1')
+    return users,ratings,movies
